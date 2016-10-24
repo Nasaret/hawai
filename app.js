8fb1e41c508498aaa33770314a8346a47766e7e0
nikia <nsovet@laposte.net>
2016-10-24 11:46:50 +0200

    +html

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 (function(){
-  var app = angular.module('hawai', ["ngRoute"]);
+  var app = angular.module('hawai', []);
 
   app.controller('produitController', function(){
-  this.produit = dataProduit;
-})
+    this.produit = dataProduit;
+  })
 
-var dataProduit = [{
- photoProduit : "https://pixabay.com/en/surfboard-beach-sand-ocean-sea-690904/",
- textProduit : "Planche discount",
- descriptifProduit : "Planche à peine usée",
- prixProduit : "100"
-},{
-  photoProduit = "https://pixabay.com/en/surfboards-beach-manly-vacation-320077/",
-  textProduit = "Lot de planche",
+  var dataProduit = [{
+    photoProduit : "images/planche.jpg",
+   textProduit : "Planche discount",
+   descriptifProduit : "Planche à peine usée",
+   prixProduit : "100"
+ },{
+  photoProduit : "images/bcpplanche.jpg",
+  textProduit : "Lot de planche",
   descriptifProduit : "Un lot de planche pour toute une vie",
   prixProduit : "1000"
 },{
-  photoProduit = "https://pixabay.com/en/corralejo-fuerteventura-1155451/",
-  textProduit = "Planche titane",
+  photoProduit : "images/requin.jpg",
+  textProduit : "Planche titane",
   descriptifProduit : "Une planche indescrutible, et vous ?",
   prixProduit : "2000"
 },{
-  photoProduit = "https://pixabay.com/en/surfing-surf-board-ocean-pacific-969476/",
-  textProduit = "Planche XL",
+  photoProduit : "images/fille.jpg",
+  textProduit : "Planche XL",
   descriptifProduit : "Plus gros que la vague",
   prixProduit : "500"
 }];
